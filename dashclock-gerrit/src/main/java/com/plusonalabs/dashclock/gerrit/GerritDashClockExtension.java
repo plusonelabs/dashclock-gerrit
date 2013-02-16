da2e0365198d7f0274bf3149aafcf5bb076b8e7d
Moritz Post <moritzpost@gmail.com>
2013-02-16 12:48:07 +0100

    Initial commit.

---
+++ dashclock-gerrit/src/main/java/com/plusonalabs/dashclock/gerrit/GerritDashClockExtension.java
@@ -0,0 +1,12 @@
+package com.plusonalabs.dashclock.gerrit;
+
+import com.google.android.apps.dashclock.api.DashClockExtension;
+
+public class GerritDashClockExtension extends DashClockExtension {
+
+	@Override
+	protected void onUpdateData(int arg0) {
+
+	}
+
+}
